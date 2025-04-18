2623d320f94664306ddf43f3ad453d9e0a982e92
NazaninFatemi <nfatemi53@gmail.com>
2025-04-17 22:46:12 -0700

    Update Exercises.java

--- AP1403 - WarmUp/src/main/java/Exercises.java
+++ AP1403 - WarmUp/src/main/java/Exercises.java
@@ -1,56 +1,48 @@
+//import java.util.Scanner;
+
 public class Exercises {
 
-    /*
-        complete this function to check if the input number is prime or not
-     */
+    /* complete this function to check if the input number is prime or not */
     public boolean isPrime(long n) {
-
-        // todo
-        int sum;
-        for(int i=1 ; i<n ; i++){
-            if(n%i==0) sum++;
+        if (n < 2) {
+            return false;
         }
-        if(sum==0)return true;
-        else return false; 
-        
-    }
-
-    /*
-        implement an algorithm to find out the index of input number in a fibonacci sequence starting from 0, 1
-        e.g. 0, 1, 1, 2, 3, 5, ...
-        indices start from 0, e.g. 3 is the index 4 of this sequence
-        if the input is not a fibonacci number with description above, return -1
-     */
-    public long fibonacciIndex(long n) {
-
-        // todo
-        int index=0;
-        int a=0;
-        int b=1;
-    
-        if (n<0)return -1;
-        if(n==0)return 0;
-        index++;
-        if(n==1)return index;
-        index++;
-        while (b <= num) {
-            int nextFib = a + b;
-            a = b;
-            b = nextFib;
-            index++;
-
-            if (b == num) {
-                return index;
+        for (int i = 2 ; i <= n / 2 ; i++) {
+            if (n % i == 0) {
+                return false;
             }
         }
-
-        return -1;
-
-
+        return true;
     }
 
-    /*
-        you should create a triangle with "*" and return a two-dimensional array of characters based on that
+    /*  implement an algorithm to find out the index of input number in a fibonacci sequence starting from 0, 1
+        e.g. 0, 1, 1, 2, 3, 5, ...
+        indices start from 0, e.g. 3 is the index 4 of this sequence
+        if the input is not a fibonacci number with description above, return -1 */
+    public long fibonacciIndex(long n) {
+        int a = 1, b = 2, c;
+        int index = 3;
+        if (n == 0) {
+            return 0;
+        }
+        else if (n == 1) {
+            return 1;
+        }
+        else {
+            while (b <= n) {
+                if (b == n) {
+                    return index;
+                }
+                index++;
+                c = a + b;
+                a = b;
+                b = c;
+            }
+        }
+        return -1;
+    }
+
+    /*  you should create a triangle with "*" and return a two-dimensional array of characters based on that
         the triangle's area is empty, which means some characters should be " "
 
         example 1, input = 3:
@@ -65,35 +57,24 @@ public class Exercises {
         *  *
         *****
 
-        the output has to be a two-dimensional array of characters, so don't just print the triangle!
-     */
-   public char[][] generateTriangle(int n)
-    {
-        char[][] triangle = new char[n][];
-        for(int i = 0; i < n; i++)
-        {
-            triangle[i] = new char[i+1];
+        the output has to be a two-dimensional array of characters, so don't just print the triangle! */
+    public char[][] generateTriangle(int n) {
+        char[][] triangle = new char [n][];
+        for (int i = 0 ; i < n ; i++) {
+            triangle[i] = new char [i + 1];
         }
-        for(int i = 0; i < n; i++) {
-            for (int j = 0; j < triangle[i].length; j++)
-            {
-                if(i == n-1)
-                {
-                    for(int k = 0; k<n; k++)
-                    {
-                        triangle[i][k] = '*';
-                    }
+        for (int i = 0 ; i < n ; i++) {
+            for (int j = 0 ; j <= i ; j++) {
+                if (j == 0 || j == i){
+                    triangle[i][j] = '*';
                 }
-                else
-                {
-                    if(j == 0 || j == triangle[i].length - 1)
-                    {
-                        triangle[i][j] = '*';
-                    }
-                    else 
-                        triangle[i][j] = ' ';
+                else {
+                    triangle[i][j] = ' ';
                 }
             }
+        }
+        for (int i = 0 ; i < n ; i++) {
+            triangle[n - 1][i] = '*';
         }
         return triangle;
     }
